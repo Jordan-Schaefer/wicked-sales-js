7328b47399694ea3c886fcbfce3e9d696ce7c564
Jordan Schaefer <jschaef99@gmail.com>
2020-04-23 17:10:22 -0700

    get data from server

--- server/index.js
+++ server/index.js
@@ -19,6 +19,25 @@ app.get('/api/health-check', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.get('/api/products', (req, res, next) => {
+  const sql = `
+  select "productId",
+         "name",
+         "price",
+         "image",
+         "shortDescription"
+    from "products"
+  `;
+  db.query(sql)
+    .then(response => {
+      res.status(200).json(response.rows);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'an error hs occurred' });
+    });
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
