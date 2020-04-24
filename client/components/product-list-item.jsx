0c01d6ee86f733ec96e2534d614f05cbfee3ccef
Jordan Schaefer <jschaef99@gmail.com>
2020-04-23 17:51:11 -0700

    Header component created

---
+++ client/components/product-list-item.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ProductListItem extends React.Component {
+  render() {
+    return (
+      <>
+        <div>
+          <img src="" alt=""/>
+          <h4></h4>
+          <h5></h5>
+          <h5></h5>
+        </div>
+      </>
+    );
+  }
+}
+
+export default ProductListItem;
