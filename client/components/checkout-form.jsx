2ab0da1f71168bbddf44b2a6b23e923a18addf33
Jordan Schaefer <jschaef99@gmail.com>
2020-04-29 14:32:24 -0700

    form layout

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -11,13 +11,22 @@ class CheckoutForm extends React.Component {
     return (
       <>
         <Header />
-        <form action="" className='container'>
-          <label htmlFor="name">Name</label>
-          <input type="text" name='name' placeholder='Name'/>
-          <label htmlFor="creditCard">Credit Card Number</label>
-          <input type="text" name="creditCard" placeholder='card number'/>
-          <label htmlFor="address">Address</label>
-          <textarea name="address" cols="30" rows="10" placeholder='address'></textarea>
+        <form action="" className='container mt-4'>
+          <div className='form-group'>
+            <label htmlFor="name">Name</label>
+            <input type="text" name='name' placeholder='first name - last name' className='form-control'/>
+          </div>
+          <div className='form-group'>
+            <label htmlFor="creditCard">Credit Card Number</label>
+            <input type="text" name="creditCard" placeholder='card number' className='form-control'/>
+          </div>
+          <div className='form-group'>
+            <label htmlFor="address">Address</label>
+            <textarea name="address" cols="20" rows="10" placeholder='address' className='form-control'></textarea>
+          </div>
+          <div>
+            <button className='btn btn-success'>Submit</button>
+          </div>
         </form>
       </>
     );
