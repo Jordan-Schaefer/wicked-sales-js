499a6d597094acb1ca2aa2d472f2a6eb44d2ab46
Jordan Schaefer <jschaef99@gmail.com>
2020-06-16 14:55:17 -0700

    fixed typo in cart summary

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -31,7 +31,7 @@ class CartSummary extends React.Component {
       return (
         <>
           <div className='container'>
-            <a onClick={() => this.props.view('catalog', {})} href="">&#60; Bact to catalog</a>
+            <a onClick={() => this.props.view('catalog', {})} href="">&#60; Back to catalog</a>
             <h1>There are no items in your cart</h1>
           </div>
         </>
